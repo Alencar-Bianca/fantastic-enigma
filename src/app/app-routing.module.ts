0996098ebc89dd80c28d3a43ba2f447325685937
Alencar-Bianca <alencar-bianca@hotmail.com>
2022-01-01 21:13:01 -0300

    Create the pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { ProductsComponent } from './products/products.component';
+import { FormComponent } from './form/form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent, pathMatch: 'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'form', component:FormComponent},
+  {path: 'products', component:ProductsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormComponent } from './form/form.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    FormComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
